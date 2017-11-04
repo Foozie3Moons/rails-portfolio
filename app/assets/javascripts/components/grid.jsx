a5372799c28e84f86c4891f703273c5fe24f6393
foozie3moons <matthew.bell.290@gmail.com>
2017-11-04 10:33:50 -0700

    added d3 hex grid pattern to the background. still needs some tweeking, such as allowing pointer events through the page, and allowing the background to scroll with the page

---
+++ app/assets/javascripts/components/grid.jsx
@@ -0,0 +1,11 @@
+class Grid extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return(<h1 className='title is-1'>Hello, from {this.props.name}</h1>)
+  }
+}
