8a807128d40a38cd48b8d85bb107b03962a5b091
foozie3moons <matthew.bell.290@gmail.com>
2017-11-05 13:14:20 -0800

    attempting to configure email again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'fc4d405e910b73',
+    :password => 'd2e59b6ed74c95',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -29,7 +39,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
