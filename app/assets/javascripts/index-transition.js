fc6f1a1d38351e8780bc6017fe448fa7aa9999a5
foozie3moons <matthew.bell.290@gmail.com>
2017-11-06 14:01:35 -0800

    added transitions to index page

---
+++ app/assets/javascripts/index-transition.js
@@ -0,0 +1,18 @@
+$(document).on('turbolinks:load', function() {
+  $('#index-persistent').hide();
+  $('#index-persistent-text').css('visibility','hidden');
+  $('#about').css('visibility','hidden');
+  $('#portfolio').hide();
+
+  setTimeout(function () {
+    $('#index-persistent').show().addClass('animated fadeIn');
+  }, 250);
+  setTimeout(function () {
+    $('#index-persistent-text').css('visibility','visible').addClass('animated fadeInUp');
+  }, 2000);
+  $('#portfolio').show().addClass('animated fadeIn');
+  $('#contact').show().addClass('animated fadeIn');
+  if ($('#persistent-container').hasClass('animated')) {
+    $('#persistent-container').removeClass('animated');
+  }
+});
