cc3aeb25d9be71517637dafdcea960b1790f2170
foozie3moons <matthew.bell.290@gmail.com>
2017-11-09 18:34:55 -0800

    removing what crashed the server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :redirect_if_old
-
-  protected
-
-  def redirect_if_old
-    if request.host == 'mjbell.herokuapp.com'
-      redirect_to "#{request.protocol}mjbell.io#{request.fullpath}", :status => :moved_permanently 
-    end
-  end
 end
