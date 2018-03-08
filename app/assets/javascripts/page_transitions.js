ac5b65f3097557b21ae4282f13830f3aea533b73
foozie3moons <matthew.bell.290@gmail.com>
2018-03-08 10:05:32 -0800

    adding remaining spinner files

---
+++ app/assets/javascripts/page_transitions.js
@@ -0,0 +1,7 @@
+// document.addEventListener('page:change', function() {
+//   document.querySelector('.spinner-container').className += ' animated fadeIn';
+// });
+//
+// document.addEventListener('page:fetch', function() {
+//   document.querySelector('.spinner-container').className += ' animated fadeOut';
+// });
--- app/assets/javascripts/spinner.js
+++ app/assets/javascripts/spinner.js
@@ -1,9 +1,15 @@
 
 // show spinner on AJAX start
-$(document).on("turbolinks:before-visit", function(){
-  $(".spinner-container").show();
+$(document).on("turbolinks:click", function(){
+  console.log($('.spinner-container').css('display'));
+  $(".spinner-container").show(function() {
+    console.log($('.spinner-container').css('display'));
+  });
 });
 
 $(document).on("turbolinks:load", function(){
-  $(".spinner-container").fadeOut(1000);
+  // console.log($('.spinner-container').css('display'));
+  // $(".spinner-container").fadeOut(1000, function() {
+  //   console.log($('.spinner-container').css('display'));
+  // });
 });
