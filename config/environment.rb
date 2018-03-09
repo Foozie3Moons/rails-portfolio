cccf0357d7b2ad8e3dc57c411b67691bd049fe2f
foozie3moons <matthew.bell.290@gmail.com>
2018-03-08 20:14:13 -0800

    updating environment variable for sharegrid username;

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_EMAIL'],
+  :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'mjbell.io',
   :address => 'smtp.sendgrid.net',
